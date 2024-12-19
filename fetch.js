9613e82e73bd7e0dd461c850e55f7a6882c7972a
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-19 10:20:39 +0600

    api test name done

--- fetch.js
+++ fetch.js
@@ -1,1 +1,15 @@
+function username() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayData(data))
+}
 
+function displayData(data) {
+    for (const i of data) {
+        const nam = i.name;
+        const add = document.getElementById('ul')
+        const main = document.createElement('li')
+        main.innerText = nam
+        add.appendChild(main)
+    }
+}
